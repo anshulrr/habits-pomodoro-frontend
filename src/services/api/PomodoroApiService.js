24f9cfdea2886857381d06396d96520da05983cc
anshul <ansh14j@gmail.com>
2023-05-05 20:33:33 +0530

    minor change: passing request data as params

--- src/services/api/PomodoroApiService.js
+++ src/services/api/PomodoroApiService.js
@@ -3,8 +3,12 @@ import { apiClient } from "./ApiClient";
 export const createPomodoroApi
     = (pomodoro, task_id) => apiClient.post(`/pomodoros?task_id=${task_id}`, pomodoro)
 
+// export const pausePomodoroApi
+//     = (id, pomodoro) => apiClient.put(`/pomodoros/${id}`, pomodoro)
+
+// passing data as params
 export const pausePomodoroApi
-    = (id, pomodoro) => apiClient.put(`/pomodoros/${id}`, pomodoro)
+    = (id, pomodoro) => apiClient.put(`/pomodoros/${id}?timeElapsed=${pomodoro.timeElapsed}&status=${pomodoro.status}`)
 
 export const getPomodorosApi
     = () => apiClient.get(`/pomodoros`)
