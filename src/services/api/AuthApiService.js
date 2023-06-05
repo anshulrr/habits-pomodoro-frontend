727790207b9f9e6a87384277c16927d96361a2e8
anshul <ansh14j@gmail.com>
2023-06-05 11:09:31 +0530

    sending usernmae password in request body, instead of header

--- src/services/api/AuthApiService.js
+++ src/services/api/AuthApiService.js
@@ -7,9 +7,12 @@ import { apiClient } from "./ApiClient";
 //         }
 //     })
 
+// export const executeJwtAuthenticationService
+//     = (username, password) => apiClient.post(`/authenticate`, {}, {
+//         headers: {
+//             Authorization: 'Basic ' + window.btoa(username + ":" + password)
+//         }
+//     })
+
 export const executeJwtAuthenticationService
-    = (username, password) => apiClient.post(`/authenticate`, {}, {
-        headers: {
-            Authorization: 'Basic ' + window.btoa(username + ":" + password)
-        }
-    })
+    = (username, password) => apiClient.post(`/authenticate`, { username, password })
