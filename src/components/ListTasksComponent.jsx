8d3113a512dc2f40000ee5807f9b75194b831e8b
anshul <ansh14j@gmail.com>
2023-07-06 11:57:54 +0530

    moving pomodoros component to main component instead of navigating to new url

--- src/components/ListTasksComponent.jsx
+++ src/components/ListTasksComponent.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams, useLocation } from "react-router-dom";
 import { createPomodoroApi } from "../services/api/PomodoroApiService";
 import { retrieveAllTasks } from "../services/api/TaskApiService";
 import { useAuth } from "../services/auth/AuthContext";
+import PomodoroComponent from "./PomodoroComponent";
 
 export default function ListTasksComponent({ project }) {
 
@@ -13,6 +14,8 @@ export default function ListTasksComponent({ project }) {
     const navigate = useNavigate()
 
     const [tasks, setTasks] = useState([])
+
+    const [pomodoro, setPomodoro] = useState(null)
 
     // const [message, setMessage] = useState(null)
 
@@ -37,15 +40,19 @@ export default function ListTasksComponent({ project }) {
     function createNewPomodoro(task) {
         // console.log(task.id)
 
-        const pomodoro = {
+        const pomodoro_data = {
             startTime: new Date(),
             // length: 1
         }
 
-        createPomodoroApi(pomodoro, task.id)
+        createPomodoroApi(pomodoro_data, task.id)
             .then(response => {
                 // console.log(response)
-                navigate(`/tasks/${task.id}/pomodoros/${response.data.id}/${response.data.length}`, { state: { project: project, task } })
+                task.project = project
+                response.data.task = task
+                // console.log(response.data)
+                setPomodoro(response.data)
+                // navigate(`/tasks/${task.id}/pomodoros/${response.data.id}/${response.data.length}`, { state: { project: project, task } })
             })
             .catch(error => console.log(error))
     }
@@ -53,9 +60,12 @@ export default function ListTasksComponent({ project }) {
     return (
         <div>
             <h5>{project.name}</h5>
-            {/* {message && <div className="alert alert-warning">{message}</div>} */}
             <div>
                 <small>
+                    {
+                        tasks.length === 0 &&
+                        <div className="alert alert-warning">No task is added to this project</div>
+                    }
                     <table className="table table-hover">
                         <tbody>
                             {
@@ -75,8 +85,17 @@ export default function ListTasksComponent({ project }) {
                         </tbody>
                     </table>
                 </small>
-                <div className="btn btn-success btn-sm my-5" onClick={addNewTask}>Add New Task</div>
+                <div className="btn btn-outline-success btn-sm my-5" onClick={addNewTask}>Add New Task</div>
             </div>
+
+            {
+                pomodoro !== null &&
+                <PomodoroComponent
+                    pomodoro={pomodoro}
+                    setPomodoro={setPomodoro}
+                    createNewPomodoro={createNewPomodoro}
+                ></PomodoroComponent>
+            }
         </div>
     )
 }
