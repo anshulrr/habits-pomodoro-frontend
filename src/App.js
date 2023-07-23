da8bede5c0aa3554e43ba4fa8a4abddc70d0deaf
anshul <ansh14j@gmail.com>
2023-07-23 21:52:48 +0530

    temp fix for service worker registration

--- src/App.js
+++ src/App.js
@@ -7,14 +7,28 @@ function App() {
     // Register a service worker hosted at the root of the
     // site using the default scope.
     console.log("available")
-    navigator.serviceWorker.register("service_worker.js").then(
-      (registration) => {
-        console.log("Service worker registration succeeded:", registration);
-      },
-      (error) => {
-        console.error(`Service worker registration failed: ${error}`);
-      },
-    );
+
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      // code to remove registraions after service worker change in a deployment
+      for (let registration of registrations) {
+        // console.log('unregitering', registration)
+        // registration.unregister();
+      }
+      console.log(registrations.length)
+
+      if (registrations.length === 0) {
+        navigator.serviceWorker.register("service_worker.js").then(
+          (registration) => {
+            console.log("Service worker registration succeeded:", registration);
+          },
+          (error) => {
+            console.error(`Service worker registration failed: ${error}`);
+          },
+        );
+      }
+
+    });
+
   } else {
     console.error("Service workers are not supported.");
   }
