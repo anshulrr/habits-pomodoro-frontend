6e84190172fe582099f52a7efa55f79049d86ff5
anshul <ansh14j@gmail.com>
2023-08-03 17:16:46 +0530

    show user image in the header

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -5,6 +5,7 @@ export default function HeaderComponent() {
 
     const authContext = useAuth()
     const isAuthenticated = authContext.isAuthenticated
+    const user = authContext.user;
 
     function logout() {
         authContext.logout()
@@ -15,29 +16,67 @@ export default function HeaderComponent() {
             <div className="container">
                 <nav className="navbar navbar-expand-sm justify-content-between border-bottom border-light border-2 mb-3">
 
-                    {/* <ul className="navbar-nav">
-                            {isAuthenticated
-                                && <li className="nav-item fs-5"><Link className="nav-link" to={"/welcome/" + username}>Home</Link></li>}
-                        </ul> */}
+                    {
+                        isAuthenticated && user.photoURL &&
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link py-0" to="/">
+                                    <div className="text-center">
+                                        <img
+                                            src={user.photoURL}
+                                            style={{ borderRadius: "50%" }}
+                                            width="35rem"
+                                            height="35rem"
+                                            className="float-start"
+                                            alt={user.displayName}
+                                        // referrerPolicy='no-referrer'
+                                        />
+                                    </div>
+                                </Link>
+                            </li>
+                        </ul>
+                    }
+
                     <ul className="navbar-nav">
-                        {isAuthenticated
-                            && <li className="nav-item"><Link className="nav-link" to="/projects">Projects</Link></li>}
+                        {
+                            isAuthenticated &&
+                            <li className="nav-item"><Link className="nav-link" to="/projects">Projects</Link></li>
+                        }
                     </ul>
                     <ul className="navbar-nav">
-                        {isAuthenticated
-                            && <li className="nav-item"><Link className="nav-link" to="/stats">Stats</Link></li>}
+                        {
+                            isAuthenticated &&
+                            <li className="nav-item"><Link className="nav-link" to="/stats">Stats</Link></li>
+                        }
+                    </ul>
+                    <ul className="navbar-nav">
+                        {
+                            isAuthenticated &&
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/settings">
+                                    Settings
+                                </Link>
+                            </li>
+                        }
                     </ul>
 
                     <ul className="navbar-nav">
-                        {isAuthenticated
-                            && <li className="nav-item"><Link className="nav-link" to="/settings">Settings</Link></li>}
-                    </ul>
-
-                    <ul className="navbar-nav">
-                        {!isAuthenticated
-                            && <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>}
-                        {isAuthenticated
-                            && <li className="nav-item"><Link className="nav-link" to="/logout" onClick={logout}>Logout</Link></li>}
+                        {
+                            isAuthenticated &&
+                            <li className="nav-item">
+                                <Link className="nav-link" onClick={logout}>
+                                    Logout
+                                </Link>
+                            </li>
+                        }
+                        {
+                            !isAuthenticated &&
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">
+                                    Login
+                                </Link>
+                            </li>
+                        }
                     </ul>
                 </nav>
             </div>
