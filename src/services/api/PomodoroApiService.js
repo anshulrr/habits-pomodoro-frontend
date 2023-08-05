ec360eaacdce2df1ff099facb027331703d09e95
anshul <ansh14j@gmail.com>
2023-08-05 21:40:06 +0530

    fix

--- src/services/api/PomodoroApiService.js
+++ src/services/api/PomodoroApiService.js
@@ -6,23 +6,23 @@ export const createPomodoroApi
 // export const updatePomodoroApi
 //     = (id, pomodoro) => apiClient.put(`/pomodoros/${id}`, pomodoro)
 
-const to = new Date().getTimezoneOffset();
+const to = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 // passing data as params
 export const updatePomodoroApi
     = (id, pomodoro) => apiClient.put(`/pomodoros/${id}?timeElapsed=${pomodoro.timeElapsed}&status=${pomodoro.status}`)
 
 export const getPomodorosApi
-    = (offset, ic) => apiClient.get(`/pomodoros?offset=${offset}&include_categories=${ic}&timezone_offset=${to}`)
+    = (offset, ic) => apiClient.get(`/pomodoros?offset=${offset}&include_categories=${ic}&timezone=${to}`)
 
 export const getRunningPomodoroApi
     = () => apiClient.get(`/pomodoros/running`)
 
 export const getTasksPomodorosApi
-    = (limit, offset, ic) => apiClient.get(`/stats/tasks-time?limit=${limit}&offset=${offset}&include_categories=${ic}&timezone_offset=${to}`)
+    = (limit, offset, ic) => apiClient.get(`/stats/tasks-time?limit=${limit}&offset=${offset}&include_categories=${ic}&timezone=${to}`)
 
 export const getProjectsPomodorosApi
-    = (limit, offset, ic) => apiClient.get(`/stats/projects-time?limit=${limit}&offset=${offset}&include_categories=${ic}&timezone_offset=${to}`)
+    = (limit, offset, ic) => apiClient.get(`/stats/projects-time?limit=${limit}&offset=${offset}&include_categories=${ic}&timezone=${to}`)
 
 export const getTotalPomodorosApi
-    = (limit, offset, ic) => apiClient.get(`/stats/total-time?limit=${limit}&offset=${offset}&include_categories=${ic}&timezone_offset=${to}`)
+    = (limit, offset, ic) => apiClient.get(`/stats/total-time?limit=${limit}&offset=${offset}&include_categories=${ic}&timezone=${to}`)
