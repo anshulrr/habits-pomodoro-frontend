716d03ff43aba36fd9615445a5541e3c5756a81c
anshul <ansh14j@gmail.com>
2023-08-30 13:55:47 +0530

    popup for included categories

--- src/components/stats/DailyStatsComponent.jsx
+++ src/components/stats/DailyStatsComponent.jsx
@@ -15,41 +15,41 @@ export default function DailyStatsComponent({ pomodoroStatus, pomdorosListReload
     return (
         <div className="text-secondary">
 
-            {
-                includeCategories.length !== 0 &&
-                <div className="col-md-12">
-                    <div className="row">
-                        <div className="col-md-12 mt-2">
-                            <ListPomodorosComponent
-                                key={[reload, pomodoroStatus, pomdorosListReload]}
-                                includeCategories={includeCategories}
-                                showButtons={false}
-                            />
-                        </div >
-                    </div>
+            <div className="col-md-12">
+                <div className="row">
+                    <div className="col-md-12 mt-2">
+                        <ListPomodorosComponent
+                            key={[reload, pomodoroStatus, pomdorosListReload]}
+                            includeCategories={includeCategories}
+                            showButtons={false}
+                        />
+                    </div >
                 </div>
-            }
+            </div>
 
             <div className="col-md-12">
-                <div className="d-flex justify-content-between mb-2">
-                    <h6 className="mb-0">
-                        Included Project Categories
-                        <span className="ms-1 badge rounded-pill text-bg-secondary">
-                            {includeCategories.length}
-                            <i className="ms-1 bi bi-link-45deg" />
-                        </span>
-                    </h6>
-                    <button type="button" className="btn btn-sm btn-outline-secondary py-0 px-1" onClick={() => setShowIncludeCategories(!showIncludeCategories)}>
-                        <i className="bi bi-pencil-square" />
-                    </button>
+                <div className="mb-0 small text-center">
+                    Included Categories
+                    <span className="small ms-1 align-middle">
+                        ({includeCategories.length}<i className="bi bi-link-45deg" />)
+                    </span>
+                    <span className="small ms-1 align-middle">
+                        <i className="bi bi-pencil-square" onClick={() => setShowIncludeCategories(!showIncludeCategories)} />
+                    </span>
                 </div>
-                <div style={{ display: showIncludeCategories ? "block" : "none" }} >
-                    <CategoryChecklistComponent
-                        setIncludeCategories={setIncludeCategories}
-                        reload={reload}
-                        setReload={setReload}
-                        setShowIncludeCategories={setShowIncludeCategories}
-                    />
+
+                <div className="update-popup-2-container">
+                    <div className="update-popup-2" style={{ display: showIncludeCategories ? "block" : "none" }} >
+                        <span className="ms-1 align-middle">
+                            <i className="bi bi-x-lg" onClick={() => setShowIncludeCategories(!showIncludeCategories)} />
+                        </span>
+                        <CategoryChecklistComponent
+                            setIncludeCategories={setIncludeCategories}
+                            reload={reload}
+                            setReload={setReload}
+                            setShowIncludeCategories={setShowIncludeCategories}
+                        />
+                    </div>
                 </div>
             </div>
 
