8d77f8210bbb22535e24d959a71f5a98b403f2be
anshul <ansh14j@gmail.com>
2023-09-09 19:58:52 +0530

    ux cleanup

--- src/components/WelcomeComponent.jsx
+++ src/components/WelcomeComponent.jsx
@@ -10,13 +10,13 @@ export default function WelcomeComponent() {
 
     const { state } = useLocation();
 
-    const [project, setProject] = useState(state && state.project)
+    const [project, setProject] = useState(state && state.project);
 
     const [tasksTitle, setTasksTitle] = useState('');
+    const [showTasksFilters, setShowTasksFilters] = useState(false);
 
-
-    const [startDate, setStartDate] = useState(moment().startOf('day').toISOString())
-    const [endDate, setEndDate] = useState(moment().endOf('day').toISOString())
+    const [startDate, setStartDate] = useState(moment().startOf('day').toISOString());
+    const [endDate, setEndDate] = useState(moment().endOf('day').toISOString());
     const [isReversed, setReversed] = useState(false);
 
     function fetchUpcomingTasks() {
@@ -44,19 +44,30 @@ export default function WelcomeComponent() {
                         setProject={setProject}
                     />
 
-                    <div className="border-bottom">
-                        <h6 className="px-2">
-                            Task Filters
-                        </h6>
-                        <div className={(!project && tasksTitle === "Upcoming" ? "list-selected " : "") + "px-2 py-1 small list-row"} onClick={fetchUpcomingTasks}>
-                            <i className="px-1 bi bi-calendar-check" />
-                            Upcoming
+                    <div className="border-bottom border-2 mb-3">
+                        <div className="d-flex justify-content-between">
+                            <h6 className="px-2">
+                                Tasks Filters
+                            </h6>
+                            <button type="button" className="btn btn-sm btn-outline-secondary py-0 px-1 mb-2" onClick={() => setShowTasksFilters(!showTasksFilters)}>
+                                <i className="bi bi-pencil-square" />
+                            </button>
                         </div>
-                        <div className={(!project && tasksTitle === "Overdue" ? "list-selected " : "") + "px-2 py-1 small list-row"} onClick={fetchOverdueTasks}>
-                            <i className="px-1 bi bi-calendar-check text-danger" />
-                            Overdue
-                        </div>
+                        {
+                            showTasksFilters &&
+                            <div>
+                                <div className={(!project && tasksTitle === "Upcoming" ? "list-selected " : "") + "px-2 py-1 small list-row"} onClick={fetchUpcomingTasks}>
+                                    <i className="px-1 bi bi-calendar-check" />
+                                    Upcoming
+                                </div>
+                                <div className={(!project && tasksTitle === "Overdue" ? "list-selected " : "") + "px-2 py-1 small list-row"} onClick={fetchOverdueTasks}>
+                                    <i className="px-1 bi bi-calendar-check text-danger" />
+                                    Overdue
+                                </div>
+                            </div>
+                        }
                     </div>
+
                 </div>
                 <div className="col-lg-8">
                     {
