7d73ea2abb65ec70fa47d499865c4b44b9ed56c4
anshul <ansh14j@gmail.com>
2023-11-07 16:19:19 +0530

    ux: header css

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -12,76 +12,72 @@ export default function HeaderComponent() {
 
     return (
         <header style={{ paddingTop: "56px" }}>
-            <nav className="navbar navbar-expand-sm fixed-top justify-content-between bg-light border-bottom border-2">
+            <nav className="fixed-top bg-light border-bottom border-2">
                 <div className="container">
+                    <div className="d-flex justify-content-between header-menu">
 
-                    {
-                        isAuthenticated &&
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                {
-                                    <Link className="nav-link" to="/" state={state}>
-                                        <div className={"border-bottom border-1 " + (url === "/" ? "text-dark border-dark" : "border-light")}>
-                                            Home
-                                        </div>
-                                    </Link>
-                                }
-                            </li>
-                        </ul>
-                    }
-
-                    <ul className="navbar-nav">
                         {
                             isAuthenticated &&
-                            <li className="nav-item">
+                            <Link className={"nav-link" + (url === "/" ? " text-success" : "")} to="/" state={state}>
+                                <div className={"my-2 ms-1 border-bottom border-1 " + (url === "/" ? "text-success border-success" : "text-secondary border-light")}>
+                                    <i className="bi bi-house"></i>
+                                    <div className="button-title">
+                                        Home
+                                    </div>
+                                </div>
+                            </Link>
+                        }
+
+                        <div className="d-flex">
+                            {
+                                isAuthenticated &&
                                 <Link className="nav-link" to="/stats" state={state}>
-                                    <div className={"border-bottom border-1 " + (url.includes("stats") ? "text-dark border-dark" : "border-light")}>
-                                        Stats
+                                    <div className={"my-2 border-bottom border-1 " + (url.includes("stats") ? "text-success border-success" : "text-secondary border-light")}>
+                                        <i className="bi bi-graph-up"></i>
+                                        <div className="button-title">
+                                            Stats
+                                        </div>
                                     </div>
                                 </Link>
-                            </li>
-                        }
-                    </ul>
-
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
+                            }
+                            {
+                                isAuthenticated &&
+                                <Link className="nav-link" to="/settings" state={state}>
+                                    <div className={"my-2 ms-3 border-bottom border-1 " + (url.includes("settings") ? "text-success border-success" : "text-secondary border-light")}>
+                                        <i className="bi bi-gear"></i>
+                                        <div className="button-title">
+                                            Settings
+                                        </div>
+                                    </div>
+                                </Link>
+                            }
                             {
                                 isAuthenticated && user.photoURL &&
-                                <Link className="nav-link py-0" to="/settings" state={state}>
-                                    <div className={"text-center border-bottom border-1 " + (url.includes("settings") ? "text-dark border-dark" : "border-light")}>
-                                        <img
-                                            src={user.photoURL}
-                                            style={{ borderRadius: "50%" }}
-                                            width="33rem"
-                                            height="33rem"
-                                            className={(url.includes("settings") ? "border-dark" : "border-light-subtle") + " border border-1"}
-                                            alt={user.displayName}
-                                        // referrerPolicy='no-referrer'
-                                        />
-                                    </div>
-                                </Link>
+                                <div className="ms-2 text-center" style={{ paddingTop: "8px" }}>
+                                    <img
+                                        src={user.photoURL}
+                                        style={{ borderRadius: "50%" }}
+                                        width="40rem"
+                                        height="40rem"
+                                        className={"border-light-subtle border border-1"}
+                                        alt={user.displayName}
+                                    // referrerPolicy='no-referrer'
+                                    />
+                                </div>
                             }
-                            {
-                                isAuthenticated && !user.photoURL &&
-                                <Link className={"nav-link" + (url.includes("settings") ? " text-dark" : "")} to="/settings" state={state}>
-                                    <div className={"border-bottom border-1 " + (url.includes("settings") ? "text-dark border-dark" : "border-light")}>
-                                        Settings
-                                    </div>
-                                </Link>
-                            }
-                        </li>
-                    </ul>
+                        </div>
 
-                    {
-                        !isAuthenticated &&
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/login" state={{}}>
-                                    Login
-                                </Link>
-                            </li>
-                        </ul>
-                    }
+                        {
+                            !isAuthenticated &&
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/login" state={{}}>
+                                        Login
+                                    </Link>
+                                </li>
+                            </ul>
+                        }
+                    </div>
                 </div>
             </nav>
 
