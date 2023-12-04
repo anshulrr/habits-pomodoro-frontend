6ebc20197592c81c32559084eb2ff32d4361e3a8
anshul <ansh14j@gmail.com>
2023-12-04 19:11:58 +0530

    showing project category color during project create / update

--- src/components/features/projects/ProjectComponent.jsx
+++ src/components/features/projects/ProjectComponent.jsx
@@ -4,8 +4,6 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { createProjectApi, retrieveProjectApi, updateProjectApi } from 'services/api/ProjectApiService'
 import { retrieveAllProjectCategoriesApi } from "services/api/ProjectCategoryApiService";
 
-import { Formik, ErrorMessage, Field } from 'formik'
-
 export default function ProjectComponent() {
 
     const { id } = useParams()
@@ -17,6 +15,8 @@ export default function ProjectComponent() {
     const [pomodoroLength, setPomodoroLength] = useState(0)
     const [priority, setPriority] = useState(1)
     const [projectCategories, setProjectCategories] = useState([])
+    const [categoryColor, setCategoryColor] = useState('818181');
+    const [errors, setErrors] = useState({})
 
     const navigate = useNavigate()
     const { state } = useLocation();
@@ -26,7 +26,6 @@ export default function ProjectComponent() {
             (() => {
                 // console.debug('re-render ProjectComponents')
                 retrieveProjectCategories()
-                retrieveProject()
             })();
         }, [] // eslint-disable-line react-hooks/exhaustive-deps
     )
@@ -36,11 +35,12 @@ export default function ProjectComponent() {
         retrieveAllProjectCategoriesApi(10, 0)
             .then(response => {
                 setProjectCategories(response.data)
+                retrieveProject(response.data)
             })
             .catch(error => console.error(error.message))
     }
 
-    function retrieveProject() {
+    function retrieveProject(categories) {
 
         if (parseInt(id) === -1) {
             return;
@@ -53,23 +53,33 @@ export default function ProjectComponent() {
                 setColor(response.data.color)
                 setPomodoroLength(response.data.pomodoroLength)
                 setPriority(response.data.priority)
-                // todo: set project projectCategory id: done
                 setProjectCategoryId(response.data.projectCategoryId)
+                for (const category of categories) {
+                    if (category.id === response.data.projectCategoryId) {
+                        setCategoryColor(category.color);
+                    }
+                }
             })
             .catch(error => console.error(error.message))
     }
 
-    function onSubmit(values) {
+    function handleSubmit(error) {
+        error.preventDefault();
+
         // console.debug({ name, description, projectCategoryId, color, pomodoroLength })
         // console.debug(values)
         const project = {
             id,
-            name: values.name,
-            description: values.description,
-            color: values.color,
-            pomodoroLength: values.pomodoroLength,
-            priority: values.priority,
-            projectCategoryId: values.project_category_id
+            name,
+            description,
+            color,
+            pomodoroLength,
+            priority,
+            projectCategoryId
+        }
+
+        if (!validate(project)) {
+            return;
         }
 
         if (parseInt(id) === -1) {
@@ -92,88 +102,143 @@ export default function ProjectComponent() {
         }
     }
 
-    function validate(values) {
+    function validate(project) {
         let errors = {}
-        if (values.name.length < 2) {
+        let result = true;
+        if (project.name.length < 2) {
             errors.name = 'Enter atleast 2 characters'
+            result = false;
         }
-        if (values.project_category_id === 0) {
-            errors.project_category_id = 'Select a category'
+        if (project.projectCategoryId === 0) {
+            errors.projectCategoryId = 'Select a category'
+            result = false;
         }
-        if (values.pomodoroLength === '' || values.pomodoroLength < 0) {
+        if (project.pomodoroLength === '' || project.pomodoroLength < 0) {
             errors.pomodoroLength = 'Enter zero or positive value'
+            result = false;
         }
-        if (values.priority === '' || values.priority < 1) {
+        if (project.priority === '' || project.priority < 1) {
             errors.priority = 'Enter positive value'
+            result = false;
         }
-        // console.debug(values)
-        return errors
+        setErrors(errors);
+        // console.debug(errors)
+        return result;
     }
 
     return (
         <div className="container mt-3">
             <h6>Enter Project Details </h6>
             <div>
-                <Formik initialValues={{ name, description, color, pomodoroLength, priority, project_category_id: projectCategoryId }}
-                    enableReinitialize={true}
-                    onSubmit={onSubmit}
-                    validate={validate}
-                    validateOnChange={false}
-                    validateOnBlur={false}
-                >
-                    {
-                        ({ errors, handleSubmit }) => (
-                            <form onSubmit={handleSubmit}>
-                                <div className="row small text-start text-secondary">
-                                    <div className="col-lg-4 mb-3">
-                                        <label htmlFor="name">Project Name</label>
-                                        <Field type="text" className="form-control form-control-sm" id="name" name="name" placeholder="Project Name" />
-                                        <ErrorMessage name="name" component="div" className="small text-danger" />
-                                    </div>
-                                    <div className="col-lg-8 mb-3">
-                                        <label htmlFor="description">Description</label>
-                                        <Field type="text" className="form-control form-control-sm" id="description" name="description" placeholder="Description" />
-                                    </div>
-                                    <div className="col-lg-4 mb-3">
-                                        <label htmlFor="color">Project Color</label>
-                                        <Field type="color" className="form-control form-control-sm" id="color" name="color" placeholder="color" />
-                                    </div>
-                                    <div className="col-lg-4 mb-3">
-                                        <label htmlFor="pomodoroLength">Default Pomodoro Length <i className="bi bi-hourglass" /></label>
-                                        <Field type="number" className="form-control form-control-sm" min="0" id="pomodoroLength" name="pomodoroLength" placeholder="Default Pomodoro Length" />
-                                        <small>(To use general settings, set length to zero)</small>
-                                        {errors.pomodoroLength && <div className="text-danger small">{errors.pomodoroLength}</div>}
-                                    </div>
-                                    <div className="col-lg-4 mb-3">
-                                        <label htmlFor="priority">Priority <i className="bi bi-arrow-up" /></label>
-                                        <Field type="number" className="form-control form-control-sm" min="1" id="priority" name="priority" placeholder="Priority" />
-                                        {errors.priority && <div className="text-danger small">{errors.priority}</div>}
-                                    </div>
-                                    <div className="col-lg-4 mb-3">
-                                        <label htmlFor="project_category_id">Project Category <i className="bi bi-link-45deg" /></label>
-                                        <Field as="select" className="form-select form-select-sm" id="project_category_id" name="project_category_id">
-                                            {/* disabled option with value 0 for dropdown to avoid confusion of initial selection */}
-                                            <option value="0" disabled>Select a Category</option>
-                                            {
-                                                projectCategories.map(
-                                                    projectCategory => (
-                                                        <option key={projectCategory.id} value={projectCategory.id}>{projectCategory.name}</option>
-                                                    )
-                                                )
-                                            }
-                                        </Field>
-                                        {errors.project_category_id && <div className="text-danger small">{errors.project_category_id}</div>}
-                                        {/* <ErrorMessage name="project_category_id" component="div" className="text-danger small" /> */}
-                                    </div>
-                                    <div className="col-lg-12 mb-3 text-end">
-                                        <button className="me-2 btn btn-sm btn-outline-secondary" type="button" onClick={() => navigate(-1, { state })}>Cancel</button>
-                                        <button className="btn btn-sm btn-outline-success" type="submit">Save Project</button>
-                                    </div>
-                                </div>
-                            </form>
-                        )
-                    }
-                </Formik>
+                <form onSubmit={handleSubmit}>
+                    <div className="row small text-start text-secondary">
+                        <div className="col-lg-4 mb-3">
+                            <label htmlFor="name">Project Name</label>
+                            <input
+                                type="text"
+                                className="form-control form-control-sm"
+                                id="name"
+                                name="name"
+                                placeholder="Project Name"
+                                value={name}
+                                required
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <div className="text-danger"><small>{errors.name}</small></div>
+                        </div>
+                        <div className="col-lg-8 mb-3">
+                            <label htmlFor="description">Description</label>
+                            <input
+                                type="text"
+                                className="form-control form-control-sm"
+                                id="description"
+                                name="description"
+                                placeholder="Description"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </div>
+                        <div className="col-lg-4 mb-3">
+                            <label htmlFor="color">Project Color</label>
+                            <input
+                                type="color"
+                                className="form-control form-control-sm"
+                                id="color"
+                                name="color"
+                                placeholder="color"
+                                value={color}
+                                required
+                                onChange={(e) => setColor(e.target.value)}
+                            />
+                        </div>
+                        <div className="col-lg-4 mb-3">
+                            <label htmlFor="pomodoroLength">Default Pomodoro Length <i className="bi bi-hourglass" /></label>
+                            <input
+                                type="number"
+                                className="form-control form-control-sm"
+                                min="0" id="pomodoroLength"
+                                name="pomodoroLength"
+                                placeholder="Default Pomodoro Length"
+                                value={pomodoroLength}
+                                required
+                                onChange={(e) => setPomodoroLength(e.target.value)}
+                            />
+                            <small>(To use general settings, set length to zero)</small>
+                            <div className="text-danger small">{errors.pomodoroLength}</div>
+                        </div>
+                        <div className="col-lg-4 mb-3">
+                            <label htmlFor="priority">Priority <i className="bi bi-arrow-up" /></label>
+                            <input
+                                type="number"
+                                className="form-control form-control-sm"
+                                min="1" id="priority"
+                                name="priority"
+                                placeholder="Priority"
+                                value={priority}
+                                required
+                                onChange={(e) => setPriority(e.target.value)}
+                            />
+                            <div className="text-danger small">{errors.priority}</div>
+                        </div>
+                        <div className="col-lg-4 mb-3">
+                            <label htmlFor="projectCategoryId">
+                                Project Category
+                                <i className="ms-1 bi bi-link-45deg" style={{ color: categoryColor }} />
+                            </label>
+                            <select
+                                className="form-select form-select-sm"
+                                id="projectCategoryId"
+                                name="projectCategoryId"
+                                value={projectCategoryId}
+                                onChange={(e) => {
+                                    const id = e.target.value;
+                                    setProjectCategoryId(id)
+                                    for (const category of projectCategories) {
+                                        if (category.id === parseInt(id)) {
+                                            setCategoryColor(category.color);
+                                        }
+                                    }
+                                }}
+                            >
+                                {/* disabled option with value 0 for dropdown to avoid confusion of initial selection */}
+                                <option value="0" disabled>Select a Category</option>
+                                {
+                                    projectCategories.map(
+                                        projectCategory => (
+                                            <option key={projectCategory.id} value={projectCategory.id}>{projectCategory.name}</option>
+                                        )
+                                    )
+                                }
+                            </select>
+                            <div className="text-danger small">{errors.projectCategoryId}</div>
+                            {/* <ErrorMessage name="projectCategoryId" component="div" className="text-danger small" /> */}
+                        </div>
+                        <div className="col-lg-12 mb-3 text-end">
+                            <button className="me-2 btn btn-sm btn-outline-secondary" type="button" onClick={() => navigate(-1, { state })}>Cancel</button>
+                            <button className="btn btn-sm btn-outline-success" type="submit">Save Project</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     )
