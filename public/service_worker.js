16356b47f6bd569babe920333f78bcd24b00e46a
anshul <ansh14j@gmail.com>
2024-01-09 11:08:24 +0530

    Fix: pomodoro finished notification cleanup

--- public/service_worker.js
+++ public/service_worker.js
@@ -9,11 +9,38 @@ self.onmessage = (event) => {
     }
 
     if (event.data.status === 'started') {
-        timeout = setTimeout(showNotification, 1000 * event.data.timeRemaining, event.data.task + " is completed")
+        timeout = setTimeout(showNotification, 1000 * event.data.timeRemaining, event.data.task)
     }
 }
 
-function showNotification(message) {
-    // console.debug('message for notification: ', message)
-    self.registration.showNotification(message)
+function showNotification(task) {
+    // console.debug('task for notification: ', task)
+    const notificationTitle = "Pomodoro Finished";
+    const notificationOptions = {
+        body: task,
+        icon: '/logo192.png',
+        data: { url: 'https://habitspomodoro.in/' },
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions)
 }
+
+self.addEventListener("notificationclick", (event) => {
+    event.notification.close();  // Android needs explicit close.
+
+    event.waitUntil(
+        clients.matchAll({ includeUncontrolled: true, type: 'window' }).then(windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === event.notification.data.url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(event.notification.data.url);
+            }
+        })
+    );
+});
