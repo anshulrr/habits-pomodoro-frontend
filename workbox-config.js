61dd44686ccbb0c7fa2e4921896fada902652559
anshul <ansh14j@gmail.com>
2026-03-17 01:18:24 +0530

    fix: google api key alert

--- workbox-config.js
+++ workbox-config.js
@@ -3,6 +3,6 @@ module.exports = {
 	globPatterns: [
 		'**/*.{js,txt,json,png,html,svg,ico,woff2,woff,mp3,css}'
 	],
-	swSrc: 'src/sw.js',
+	swSrc: 'src/firebase-messaging-sw.js',
 	swDest: 'build/firebase-messaging-sw.js',
 };
