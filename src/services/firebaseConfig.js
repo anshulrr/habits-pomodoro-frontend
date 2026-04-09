8bbb4590af4fafbefa2d7ba1ad657cb5b32f6f54
Anshul <ansh14j@gmail.com>
2026-04-09 17:10:40 +0530

    fix for npm audit firebase auth

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -19,7 +19,6 @@ import {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log('Firebase config', import.meta.env);
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
