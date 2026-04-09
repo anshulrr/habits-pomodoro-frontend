c8e009a617f403e0e7bc01637776f085aceee501
Anshul <ansh14j@gmail.com>
2026-04-09 10:20:58 +0530

    workbox setup with vite & jsconfig warning fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(), 
+    tsconfigPaths(),
+    VitePWA({
+      strategies: 'injectManifest', // Essential for custom logic (Firebase)
+      srcDir: 'src',                // Where your sw.js lives
+      filename: 'firebase-messaging-sw.js',            // Name of your source file
+      registerType: 'autoUpdate',
+      injectManifest: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'], // Files to precache
+      },
+      devOptions: {
+        enabled: true,               // Allows testing SW in dev mode!
+        type: 'module',
+      },
+    }),
+],
   server: {
     port: 3000, // Matches CRA's default
     open: true, // Automatically opens browser
