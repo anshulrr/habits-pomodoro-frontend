73c278fad973cf2093ae18291524c364ace246f5
Anshul <ansh14j@gmail.com>
2026-04-11 18:02:59 +0530

    prod working setup

--- capacitor.config.js
+++ capacitor.config.js
@@ -9,10 +9,12 @@ const config = {
   server: {
     // 1. For Production: This will be undefined, so it loads local files.
     // 2. For Local: You can set an environment variable when running the command.
-    // url: process.env.VITE_SERVER_URL || 'http://192.168.1.10:3000',  // Falls back to default
-    url: process.env.CAP_LIVE_RELOAD_URL, 
-    cleartext: true
+    // url: process.env.VITE_SERVER_URL || 'http://192.168.1.10:3000',  // live reload not working
+    // url: process.env.CAP_LIVE_RELOAD_URL, 
+    // cleartext: true,  // Set to true if your local server doesn't use HTTPS
+    cleartext: false, // for production, since backend will be on HTTPS
   }
 };
 
+// export default config; // doesn't work with capacitor cli for some reason, so using commonjs export
 module.exports = config; 
